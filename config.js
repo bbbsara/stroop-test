2b37f8d091105032b1d8c25fab4967bc732aa05d
bbbsara <noor.z.majeed@tu.edu.iq>
2025-11-24 20:19:59 +0300

    Refactor color definitions and update script URL

--- config.js
+++ config.js
@@ -1,16 +1,14 @@
 // إعدادات اختبار ستروب
-
-// مدة الاختبار (بالثواني)
 const TEST_DURATION = 45;
 
-// الألوان المستخدمة
 const COLORS = [
-    { name: "أحمر",   code: "red" },
-    { name: "أخضر",   code: "green" },
-    { name: "أزرق",   code: "blue" },
-    { name: "أصفر",   code: "yellow" },
-    { name: "أسود",   code: "black" }
+    { name: "أحمر", code: "red" },
+    { name: "أخضر", code: "green" },
+    { name: "أزرق", code: "blue" },
+    { name: "أصفر", code: "yellow" },
+    { name: "أسود", code: "black" }
 ];
 
-// رابط Google Apps Script لاستقبال النتائج
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyTFLrettV2lclBZm8WEFGRcs-a9RKSb55-2F3DeRt08v14FKDUaKY4W2p0iIiJB1Q/exec";
+// رابط Google Script
+const GOOGLE_SCRIPT_URL =
+"https://script.google.com/macros/s/AKfycbyTFLrettV2lclBZm8WEFGRcs-a9RKSb55-2F3DeRt08v14FKDUaKY4W2p0iIiJB1Q/exec";
